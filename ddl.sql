2680cebbe2b2daace0b30672a3b9de4b9ee9a79a
zzw <kenzzw@163.com>
2019-06-29 21:08:28 +0800

    update

--- ddl.sql
+++ ddl.sql
@@ -1,31 +1,48 @@
--- **数据库级别：**  
+﻿-- **数据库级别：**  
 --  显示所有数据库  
+show DATABASES;
 
 --  进入某个数据库  
 
+use student_examination_sys;
 --  创建一个数据库  
+CREATE DATABASE if not EXISTS student_examination_sys;
 
 --  创建指定字符集的数据库  
+CREATE DATABASE IF NOT EXISTS student_examination_sys DEFAULT CHARSET utf8 COLLATE utf8_general_ci;
 
---  显示数据库的创建信息   
+--  显示数据库的创建信息  
+
+ show tables;
 
 --  修改数据库的编码  
 
+ALTER DATABASE student_examination_sys DEFAULT CHARSET utf8 COLLATE utf8_general_ci;
 --  删除一个数据库   
 
+drop DATABASE student_examination_sys ;
 -- **表级别**
 --  修改表名
+ALTER TABLE student RENAME TO student1;
 
 --  修改字段的数据类型
+alter table student modify column name varchar(30);
 
 --  修改字段名
+ALTER TABLE student CHANGE name name1  CHAR(32) ;
 
 --  添加字段
+alter table student add test varchar(10);
 
 --  删除字段
+alter table student drop test;
 
 --  修改表的存储引擎
-
+ALTER TABLE student ENGINE=InnoDB
 --  删除表的外键约束
 
+ALTER TABLE student DROP FOREIGN KEY a
+
 --  删除一张表
+
+DROP TABLE student;
--- dml.sql
+++ dml.sql
@@ -1,7 +1,12 @@
--- 插入记录  
+﻿-- 插入记录  
+
+INSERT  INTO `student` VALUES
+('001','张三',18,'男'),
+('002','李四',20,'女');
 
 -- 修改记录  
-
+UPDATE student set name='张四' where name='张三';
 --  删除记录  
-
+delete from student where name='张四';
 -- 查询记录  
+SELECT * from student;
